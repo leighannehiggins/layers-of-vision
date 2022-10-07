e73ce9f5971c2be5b37fe2db57665ae552697672
ginestra <ginestra@users.noreply.github.com>
2022-10-07 13:40:54 +0000

    deploy: bf3c97efe282ee053e6e8067d1a9c87ba0d0f27e

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,15 @@
 // Dark/Light mode switcher
 // Ref: https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 
+// GF edits to detect system/user-agent mode
+// const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+
 const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]'
 );
+const currentTheme = localStorage.getItem("theme");
 
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark"); //add this
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light"); //add this
-  }
-}
-
-const currentTheme = localStorage.getItem("theme")
-  ? localStorage.getItem("theme")
-  : null;
-
+// Check for saved user preference, if any, on load of the website
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
 
@@ -36,3 +17,16 @@ if (currentTheme) {
     toggleSwitch.checked = true;
   }
 }
+
+// Adding the event handlers and store the user preference for future visits
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
